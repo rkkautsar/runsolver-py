018f4ff94397fb5e58bd5ccb65a62b4dabd14bad
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2019-04-10 14:25:43 +0200

    fix

--- runsolver/__init__.py
+++ runsolver/__init__.py
@@ -7,7 +7,7 @@ import subprocess
 
 def run():
     runsolver = os.path.join(os.path.dirname(__file__), "runsolver", "runsolver")
-    return subprocess.call([runsolver, *sys.argv[1:]]))
+    return subprocess.call([runsolver, *sys.argv[1:]])
 
 
 if __name__ == "__main__":
